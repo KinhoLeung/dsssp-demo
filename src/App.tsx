587497ffc1e0ef9e7f2b53ab2afcc5034d90c232
KinhoLeung <kinholeung6119@gmail.com>
2025-12-18 11:47:10 +0800

    移除NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import NavBar from './pages/components/NavBar'
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col items-center">
-      <NavBar />
     </div>
   )
 }
