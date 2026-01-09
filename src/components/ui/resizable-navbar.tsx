1ff286937b2dfbeb1e94b285c5250f3ca2dfe15e
KinhoLeung <kinholeung6119@gmail.com>
2026-01-09 17:16:08 +0800

    改造resizable-navbar导航栏的2边为直角

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -85,7 +85,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
         damping: 50,
       }}
       className={cn(
-        "relative z-[60] mx-auto hidden w-full max-w-none flex-row items-center justify-between self-start rounded-full border border-border bg-background px-4 py-2 lg:flex",
+        "relative z-[60] mx-auto hidden w-full max-w-none flex-row items-center justify-between self-start rounded-none border border-border bg-background px-4 py-2 lg:flex",
         className,
       )}
     >
@@ -134,10 +134,10 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
         boxShadow: visible
           ? "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset"
           : "none",
-        width: visible ? "90%" : "100%",
-        paddingRight: visible ? "12px" : "0px",
-        paddingLeft: visible ? "12px" : "0px",
-        borderRadius: visible ? "4px" : "2rem",
+        width: "100%",
+        paddingRight: "0px",
+        paddingLeft: "0px",
+        borderRadius: "0px",
         y: visible ? 20 : 0,
       }}
       transition={{
@@ -146,7 +146,7 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
         damping: 50,
       }}
       className={cn(
-        "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between border border-border bg-background px-0 py-2 lg:hidden",
+        "relative z-50 mx-0 flex w-full max-w-none flex-col items-center justify-between rounded-none border border-border bg-background px-0 py-2 lg:hidden",
         className,
       )}
     >
