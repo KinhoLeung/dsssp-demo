6a70efcf85450312af99d70936a14d8272d6f90f
KinhoLeung <kinholeung6119@gmail.com>
2026-01-27 12:20:54 +0800

    配置 404 兜底路由

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import DemoMode from './pages/DemoMode.tsx'
 import DeviceDemo from './pages/DeviceDemo.tsx'
 import Docs from './pages/Docs.tsx'
 import Home from './pages/Home.tsx'
+import NotFound from './pages/NotFound.tsx'
 import { RequireDeviceReady } from './routes/RequireDeviceReady.tsx'
 import './main.css'
 
@@ -49,6 +50,10 @@ export const router = createHashRouter([
       {
         path: 'demo-mode',
         element: <DemoMode />
+      },
+      {
+        path: '*',
+        element: <NotFound />
       }
     ]
   }
